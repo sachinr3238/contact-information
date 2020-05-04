9b5b41ce05ea059c6a3d67f4fc693c48d7ba4648
sachinr3238 <sachinr3238@gmail.com>
2020-05-04 20:12:21 +0530

    Add files via upload
    Contact information CRUD

---
+++ contact-information/app/components/contact/contact-controller.js
@@ -0,0 +1,54 @@
+
+angular.module('contactApp', []).controller('contactController', function ($scope) {
+    // global varibles defined
+    $scope.items = [];
+    $scope.item = {};
+
+    // add filled contact on add contact button
+    $scope.addContact = () => {
+        let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.]{4,})+\.([A-Za-z]{2,4})$/;
+        if (!reg.test($scope.item.email)) {
+            $scope.emailValid = true;
+        }
+        else {
+            if ($scope.editMode == true) {
+                $scope.items[$scope.editing] = $scope.item;
+            } else {
+                $scope.items.push($scope.item);
+
+            }
+            reset()
+        }
+    }
+
+    // deleting the row on delete icon
+    $scope.removeItem = (index) => {
+        var result = confirm("Are you sure you want to delete?")
+        if (result) {
+            $scope.items.splice(index, 1)
+        }
+    }
+
+    // edit the information on edit icon
+    $scope.editItem = (data) => {
+        $scope.editMode = true;
+        $scope.item = angular.copy(data);
+        $scope.editing = $scope.items.indexOf(data);
+    }
+
+    // phone number validation
+    $scope.numberOnly = (event) => {
+        if (isNaN(String.fromCharCode(event.keyCode))) {
+            event.preventDefault();
+        }
+    }
+
+    // reseting the fields
+    reset = () => {
+        $scope.item = {};
+        $scope.emailValid = false;
+        $scope.item.status = 'active';
+        $scope.editMode = false;
+    }
+
+});
---
+++ contact-information/readme.txt
@@ -0,0 +1,22 @@
+The project is implemented using angularJs framework
+
+Folder structure
+
+Root folder (app) 
+         |
+	Assets - Css - fonts -images - scripts > vendor
+	 |
+	Components - Contact > contact-controller.js
+			     > contact-info.html
+	 |
+	Shared - As of no empty, if requried we can add service,directive,pipe file in this
+	 |
+	index.html - Main page which has links/scripts and ng-included file.
+
+
+To run the app just open the index file.
+		OR
+http://localhost/contact-information/app/
+
+
+Note: Only the inactive status record will get deleted.
